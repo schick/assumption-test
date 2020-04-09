a94f85e0d7e6f18a8290ec5d42035f9d50106583
Maximilian Schick <maxi.f.schick@gmail.com>
2020-04-09 09:24:42 +0200

    Remove glucose

--- build.sh
+++ build.sh
@@ -9,9 +9,3 @@ git submodule update
 cd ..
 cmake -S . -B build
 cmake --build build --target assumptionTest
-
-cd ../glucose
-wget https://www.labri.fr/perso/lsimon/downloads/softwares/glucose-syrup-4.1.tgz
-tar -xf glucose-syrup-4.1.tgz
-rm glucose-syrup-4.1.tgz
-find . -type f -name '._*' -delete
